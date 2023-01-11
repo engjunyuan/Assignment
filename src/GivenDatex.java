22441d45a8e5b7526bc2ed2d0ad6a72d287bf6c5
engjunyuan <engjunyuan2003@gmail.com>
2023-01-11 15:59:54 +0800

    updated to determine WEXITSTATUS

--- src/GivenDatex.java
+++ src/GivenDatex.java
@@ -9,8 +9,8 @@ public class GivenDatex
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        LocalDateTime startDate = LocalDateTime.of(2022, 6, 1, 0, 0);
-        LocalDateTime endDate = LocalDateTime.of(2022, 6, 1, 8, 59);
+        LocalDateTime startDate = LocalDateTime.of(2022, 6, 1, 0, 0,0,0);
+        LocalDateTime endDate = LocalDateTime.of(2022, 6, 1, 8, 59,0,0);
 
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
         String read;
@@ -19,11 +19,11 @@ public class GivenDatex
 
         try {
             BufferedReader input = new BufferedReader(new FileReader("C:\\Users\\User\\OneDrive - Universiti Malaya\\WIX 1002 Fundamentals of Programming\\Sem 1 Assignment\\extracted_log"));
-            int stat0=0, stat1=0, stat2=0;
+            int stat0=0, stat1=0, stat2=0,otherstat=0;
             int status;
 
             while ((read = input.readLine()) != null) {
-                String[]line = read.split(" ");
+                String[]arr = read.split(" ");
                 String timestampString = read.substring(1, 24);
                 LocalDateTime timestamp = LocalDateTime.parse(timestampString, format);
 
@@ -34,11 +34,24 @@ public class GivenDatex
                         //jobEnded++;
                     }
                     else if (read.contains("_job_complete")&& read.contains("WEXITSTATUS")) {
-                        status = Integer.parseInt(Arrays.toString(line[3].split(" ")));
+                        status = Integer.parseInt(arr[4]);
+                    if(status==0)
+                        stat0++;
+                    else if(status==1)
+                        stat1++;
+                    else if(status==2)
+                        stat2++;
+                    else
+                        otherstat++;
                 }
             }
             input.close();
-            System.out.println(jobEnded +" "+ jobCreated);
+            System.out.println("Number of jobs created and ended within "+startDate +" - "+ endDate + " are "+ jobCreated + " jobs and " + jobEnded + " jobs respectively.");
+            System.out.println("Number of jobs done with WEXITSTATUS 0 is " + stat0 +" jobs");
+            System.out.println("Number of jobs done with WEXITSTATUS 1 is " + stat1 +" jobs");
+            System.out.println("Number of jobs done with WEXITSTATUS 2 is " + stat2 +" jobs");
+            System.out.println("Number of jobs done with WEXITSTATUS other than 0,1 and 2 is " + otherstat +" jobs");
+
         }
         catch(FileNotFoundException e){
             e.printStackTrace();
